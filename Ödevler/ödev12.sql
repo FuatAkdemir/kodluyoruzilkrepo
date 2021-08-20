d4495784e2c931febc2cc3acbbad7c90206b8ef9
Fuat Akdemir <akdemir_fuat@hotmail.com>
2021-08-20 05:31:56 +0300

    Kodluyoruz SQL Ödev 12

---
+++ Ödevler/ödev12.sql
@@ -0,0 +1,14 @@
+-- 1)
+select count(length) from film where length > (select round(avg(length), 3) from film);
+-- 489
+
+-- 2)
+select count(*) from film where rental_rate = (select max(rental_rate) from film);
+-- 336
+
+-- 3)
+select * from film where rental_rate = (select min(rental_rate) from film) and replacement_cost = (select min(replacement_cost) from film);
+
+-- 4)
+select payment.customer_id, first_name, last_name  from payment join customer on customer.customer_id = payment.customer_id where amount = (select max(amount) from payment);
+
