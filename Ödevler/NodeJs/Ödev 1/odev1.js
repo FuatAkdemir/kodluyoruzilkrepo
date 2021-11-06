513d85c9c12640e80c1c4609a05f7e8d4fe59dd8
Fuat Akdemir <akdemir_fuat@hotmail.com>
2021-11-06 04:50:45 +0300

    NodeJs Klasörü oluşturuldu ve Ödev 1 eklendi

---
+++ Ödevler/NodeJs/Ödev 1/odev1.js
@@ -0,0 +1,8 @@
+let arg = process.argv.slice(2);
+
+function daireninAlani(yaricap){
+    let alan = Math.PI * yaricap * yaricap;
+    console.log(`Yarıçapı ${yaricap} olan dairenin alanı: ${alan}`);
+}
+
+daireninAlani(arg[0] * 1);
