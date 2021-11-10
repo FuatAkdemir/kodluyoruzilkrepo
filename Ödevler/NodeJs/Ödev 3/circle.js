eec9e019abcc9728c1052de272f7b6ed60a3793d
Fuat Akdemir <akdemir_fuat@hotmail.com>
2021-11-10 04:53:50 +0300

    NodeJs Ödev 3 eklendi.

---
+++ Ödevler/NodeJs/Ödev 3/circle.js
@@ -0,0 +1,12 @@
+function circleArea(r){
+    return Math.PI*r*r;
+}
+
+function  circleCircumference(yc){
+    return Math.PI*2*yc;
+}
+
+module.exports = {
+    circleArea,
+    circleCircumference
+}
